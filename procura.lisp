9b02dff8251465cd97d94ed3b09068b25fe753f5
Nuno Martinho <nuno.m.martinho@gmail.com>
2022-12-12 19:41:30 +0000

    first-commit
    Created the main files.

---
+++ procura.lisp
@@ -0,0 +1,7 @@
+;; Deve conter a implementação de:
+;; 1. Algoritmo de Procura de Largura Primeiro (BFS)
+;; 2. Algoritmo de Procura do Profundidade Primeiro (DFS)
+;; 3. Algoritmo de Procura do Melhor Primeiro (A*)
+;; 4. Os algoritmos SMA*, IDA* e/ou RBFS (caso optem por implementar o bónus)
+
+
---
+++ projeto.lisp
@@ -0,0 +1,1 @@
+;; Carrega os outros ficheiros de código, escreve e lê ficheiros, e trata da interação com o utilizador.
