47acd0d9b51188025b0fb8c30ef53f27b9b4469d
joaortcoelho <joaortcoelho@hotmail.com>
2022-12-16 18:29:24 +0000

    add start menu

--- projeto.lisp
+++ projeto.lisp
@@ -4,3 +4,31 @@
 ;; ============ CARREGAR FICHEIROS ============
 (load "procura.lisp")
 (load "puzzle.lisp")
+
+(defun start ()
+    "Inicializa o programa"
+    (menu)
+    (let ((option (read)))
+        (if 
+            (or (not (numberp option)) (< option 1) (> option 2))
+                '((format t "Insira uma opção válida!") (start))
+            (ecase option
+                ('1 ())
+                ('2 (format t "Adeus!"))
+            )
+        )
+    )
+)
+
+(defun menu ()
+    "Mostra o menu inicial"
+    (format t "Loading game...")
+    (sleep 2)
+    (format t "~%o                           o")
+    (format t "~%      - Dots and Boxes -     ")
+    (format t "~%                             ")
+    (format t "~%   1 - Escolher um problema  ")
+    (format t "~%   2 - Sair                  ")
+    (format t "~%o                           o")
+    (format t "~%~%>> ")
+)
