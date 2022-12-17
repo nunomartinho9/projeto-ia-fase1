eafb30d476d92ebeaa72f511c9e0336978ab24b1
joaortcoelho <joaortcoelho@hotmail.com>
2022-12-17 15:11:52 +0000

    add visualizar tabuleiros no menu

--- projeto.lisp
+++ projeto.lisp
@@ -8,17 +8,25 @@
 
 ;; ============= START =============
 
-(defun start ()
+(defun iniciar ()
 "Inicializa o programa"
     (menu)
-    (let ((option (read)))
+    (let ((opcao (read)))
         (if 
-            (or (not (numberp option)) (< option 1) (> option 2))
-                '((format t "Insira uma opção válida!") (start))
-            (ecase option
-                ('1 ())
-                ('2 (format t "Adeus!"))
-            )
+            (or (not (numberp opcao)) (< opcao 1) (> opcao 3))
+                (progn (format t "Escolha uma opcao valida!") (iniciar))
+                (ecase opcao
+                    ('1 (progn 
+                            (let ((tabuleiro (opcao-tabuleiro 'iniciar))) 
+                                (if (listp tabuleiro) (print-tabuleiro (second tabuleiro)))
+                            )
+                            (iniciar)
+                        )
+                    )
+                    ('2 ()
+                    )
+                    ('3 (format t "Obrigado por jogar!"))
+                )
         )
     )
 )
@@ -28,15 +36,15 @@
 (defun menu ()
 "Mostra o menu inicial"
     (progn
-        (format t "Loading game...~%")
+        (format t "~%A carregar jogo...~%")
         (sleep 1)
-        (format t "~%o                           o")
-        (format t "~%      - Dots and Boxes -     ")
-        (format t "~%                             ")
-        (format t "~%   1 - Visualizar problemas  ")
-        (format t "~%   2 - Escolher um problema  ")
-        (format t "~%   3 - Sair                  ")
-        (format t "~%o                           o")
+        (format t "~%o                              o")
+        (format t "~%|      - Dots and Boxes -      |")
+        (format t "~%|                              |")
+        (format t "~%|   1 - Visualizar problemas   |")
+        (format t "~%|   2 - Escolher um problema   |")
+        (format t "~%|   3 - Sair                   |")
+        (format t "~%o                              o")
         (format t "~%~%>> ")
     )
 )
@@ -54,12 +62,56 @@
     )
 )
 
+(defun tabuleiros-menu (&optional (i 1) (problemas (ler-tabuleiros)))
+"Mostra os tabuleiros disponíveis no menu"
+    (cond ((null problemas) 
+            (progn
+                (format t "~%o                            o")
+                (format t "~%~% 0 - Voltar atras") (format t "~%~%>> ")
+            )
+        )
+        (T (progn
+                (if (= i 1) 
+                    (progn 
+                        (format t "~%o                            o")
+                        (format t "~%|    Escolha o tabuleiro:    |")
+                    )
+                )
+                (format t "~%|    ~a - Tabuleiro ~a         |" i i)
+                (tabuleiros-menu (+ i 1) (cdr problemas))
+            )
+        )
+    )
+)
+
+(defun opcao-tabuleiro (menuVoltar)
+"Escolhe um tabuleiro do menu"
+    (progn (tabuleiros-menu)
+        (let ((opcao (read)))
+            (cond ((equal opcao '0) (funcall menuVoltar))
+                  ((not (numberp opcao)) (progn (format t "Escolha uma opção válida~%")))
+                  (T
+                    (let ((lista (ler-tabuleiros)))
+                        (if (or (< opcao 0) (> opcao (length lista)))
+                            (progn 
+                                (format t "Escolha uma opcao valida!") (ler-tabuleiros menuVoltar)
+                            )
+                            (list opcao (nth (- 1 opcao) lista))
+                        )
+                    )
+                  )
+            )
+        )
+    )
+)
+
 ;; (print-tabuleiro (ler-tabuleiros))
 (defun print-tabuleiro (tabuleiro &optional (stream t))
 "Imprime um tabuleiro do ficheiro problemas.dat"
     (not (null (mapcar #'(lambda (l)
         (format stream "~%~t~t ~a" l)) tabuleiro))
     )
+    (format t "~%")
 )
 
 ;; (print-tabuleiros (ler-tabuleiros))
@@ -68,4 +120,5 @@
     (not (null (mapcar #'(lambda (tabuleiro)
         (format stream "~%~t~t ~a" (print-tabuleiro tabuleiro))) tabuleiros))
     )
+    (format t "~%")
 )
