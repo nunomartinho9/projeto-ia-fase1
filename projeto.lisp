15ec1b8048efaacb58168f7e313cd0d11a151e1d
joaortcoelho <joaortcoelho@hotmail.com>
2022-12-17 12:22:52 +0000

    filereader & imprimir tabuleiros

--- projeto.lisp
+++ projeto.lisp
@@ -2,11 +2,14 @@
 ;; Autores: Nuno Martinho & João Coelho.
 
 ;; ============ CARREGAR FICHEIROS ============
+
 (load "procura.lisp")
 (load "puzzle.lisp")
 
+;; ============= START =============
+
 (defun start ()
-    "Inicializa o programa"
+"Inicializa o programa"
     (menu)
     (let ((option (read)))
         (if 
@@ -20,15 +23,49 @@
     )
 )
 
+;; ============= MENU =============
+
 (defun menu ()
-    "Mostra o menu inicial"
-    (format t "Loading game...")
-    (sleep 2)
-    (format t "~%o                           o")
-    (format t "~%      - Dots and Boxes -     ")
-    (format t "~%                             ")
-    (format t "~%   1 - Escolher um problema  ")
-    (format t "~%   2 - Sair                  ")
-    (format t "~%o                           o")
-    (format t "~%~%>> ")
+"Mostra o menu inicial"
+    (progn
+        (format t "Loading game...~%")
+        (sleep 1)
+        (format t "~%o                           o")
+        (format t "~%      - Dots and Boxes -     ")
+        (format t "~%                             ")
+        (format t "~%   1 - Visualizar problemas  ")
+        (format t "~%   2 - Escolher um problema  ")
+        (format t "~%   3 - Sair                  ")
+        (format t "~%o                           o")
+        (format t "~%~%>> ")
+    )
+)
+
+;; ============= TABULEIROS =============
+
+;; (ler-tabuleiros)
+(defun ler-tabuleiros ()
+"Lê os tabuleiros no ficheiro problemas.dat"
+    (with-open-file (stream "problemas.dat" :if-does-not-exist nil)
+        (do ((result nil (cons next result))
+                (next (read stream nil 'eof) (read stream nil 'eof)))
+                    ((equal next 'eof) (reverse result))
+        )
+    )
+)
+
+;; (print-tabuleiro (ler-tabuleiros))
+(defun print-tabuleiro (tabuleiro &optional (stream t))
+"Imprime um tabuleiro do ficheiro problemas.dat"
+    (not (null (mapcar #'(lambda (l)
+        (format stream "~%~t~t ~a" l)) tabuleiro))
+    )
+)
+
+;; (print-tabuleiros (ler-tabuleiros))
+(defun print-tabuleiros (tabuleiros &optional (stream t))
+"Imprime os tabuleiros do ficheiro problemas.dat"
+    (not (null (mapcar #'(lambda (tabuleiro)
+        (format stream "~%~t~t ~a" (print-tabuleiro tabuleiro))) tabuleiros))
+    )
 )
